4bf8cb9cce91ac8f3cffdd2715ee6ee307b80458
Dawid Serafin <dawidseragin96@gmail.com>
2024-10-23 13:33:02 +0200

    update inputValidation test

--- cypress/e2e/inputValidation.cy.js
+++ cypress/e2e/inputValidation.cy.js
@@ -6,4 +6,13 @@ describe('Input Value and Search Result Test', () => {
 		cy.url().should('include', '/countries/poland')
 		cy.get('h1').should('contain', 'Poland')
 	})
+	it('Should reset input value when Search button is clicked', () => {
+		cy.visit('/')
+		cy.get('#searchinput').type('Poland')
+		cy.get('button').contains('Search').click()
+		cy.url().should('include', '/countries/poland')
+		cy.get('h1').should('contain', 'Poland')
+		cy.get('h1').contains('Where in the world?').click()
+		cy.get('#searchinput').should('have.value', '')
+	})
 })
